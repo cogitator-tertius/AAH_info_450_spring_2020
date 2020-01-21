148e8b9ce33ff2c14323eb3839dda3140a17dd1c
A. Henn <aidan.henn@gmail.com>
2020-01-21 17:47:54 -0500

    Program 1
    first program from chapter 2

---
+++ chapter2/program1.cpp
@@ -0,0 +1,17 @@
+/* Program #1 - Aidan's first c++ program!
+
+When I compile this, I hope it works :)
+*/
+
+#include <iostream>
+using namespace std;
+
+// main() is where the program starts, just like c# and Java
+
+int main()
+{
+    /* code */
+    cout << "This is my first C++ program.";
+
+    return 0;
+}
